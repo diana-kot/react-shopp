8ae3a0b8573d874f770756d467d0e518897a21db
diana-kot <57452102+diana-kot@users.noreply.github.com>
2022-04-10 20:23:39 +0400

    d

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 // import ReactDOM from 'react-dom';
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 
 import "./index.scss";
 import "macro-css";
@@ -12,9 +12,9 @@ import reportWebVitals from "./reportWebVitals";
 const root = createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <Router>
       <App />
-    </BrowserRouter>
+    </Router>
   </React.StrictMode>
 );
 // ReactDOM.render(<App />, document.getElementById('root'));
